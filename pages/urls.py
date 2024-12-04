22f30b6c832f498833117ad512d0eaf15e2d28cb
William Chau <w.chau1@student.han.nl>
2024-12-04 23:33:09 +0100

    modify pages urls and views

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AboutUsView, HomePageView
+from .views import AboutUsView, HomePageView, UpdateServerView
 
 app_name = 'pages'
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('about-us/', AboutUsView.as_view(), name='about-us'),
+    path('update-server/', UpdateServerView, name='update-server'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views.decorators.http import require_http_methods
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
 class AboutUsView(TemplateView):
     template_name = 'pages/about_us.html'
+
+@require_http_methods(['GET', 'POST'])
+def UpdateServerView(request):
+    if request.method == 'POST':
+        # Do something here to update the server
+        return HttpResponse('Server updated successfully')
+    else:
+        return HttpResponse('Method not allowed')
+    
